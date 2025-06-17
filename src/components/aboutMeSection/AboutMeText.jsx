02366da44fee0f1819fcb18a218da44d83a2be39
ashishxsoni <ashishsonich12@gmail.com>
2025-06-18 01:58:50 +0530

    Fixed the AboutPage Section

--- src/components/aboutMeSection/AboutMeText.jsx
+++ src/components/aboutMeSection/AboutMeText.jsx
@@ -9,39 +9,13 @@ import { LinkPreview } from "../ui/link-preview";
 const AboutMeText = () => {
   return (
     <div className="flex flex-col text-md max-w-[90%] md:items-start sm:items-center md:text-left sm:text-center">
-      {/* <p className="text-justify">
-        I am <b>Ashish Soni</b>, a passionate and skilled full stack web
-        developer with expertise in <b>React.js</b>, <b>TailwindCSS</b>,{" "}
-        <b>Redux Toolkit</b>, and <b>Appwrite</b>. I have a solid foundation in
-        <b>JavaScript</b>, <b>Python</b>, and <b>C++</b>, and have built dynamic
-        applications including
-        <b>Enlighten</b> — a feature-rich blog app with secure authentication,
-        rich text editing, and intuitive UI/UX.
-        <br />I am proficient in <b>Data Structures and Algorithms</b> with 1000+
-        problems solved on
-        <b>LeetCode</b> and a <b>4-star rating on HackerRank</b>.
-        <br />
-        As a{" "}
-        <b>Development Team Member of Google Developers Group (GDG) GLBITM</b>,
-        I actively contribute to the developer community and recently organized
-        a workshop on building full-stack AI-integrated applications.
-        <br />I am constantly seeking to enhance user experience through
-        thoughtful <b>UI/UX design</b>
-        and efficient <b>state management</b>. I also enjoy writing{" "}
-        <b>poetry and blogs</b> in my free time.
-      </p>
- */}
-
-      <p className="text-justify">
-        I am <b>Ashish Soni</b>, a passionate and goal-oriented{" "}
-        <b>Full Stack Web Developer</b>
+      {/*  <p className="text-justify">
+        I am <b>Ashish Soni</b>, a passionate{" "}
+        <b>Full Stack Web Developer </b>
         with a strong foundation in <b>Data Structures & Algorithms</b> and{" "}
-        <b>System Design</b>. I’ve solved over <b>1300 DSA problems</b>, earned
+        <b>MERN Stack</b>. I’ve solved over <b>1300 DSA problems</b>, earned
         the <b>LeetCode Knight Badge</b>, and achieved a{" "}
-        <b>peak rating of 1856</b>. I ranked <b>725 globally</b>
-        in a LeetCode Weekly Contest and secured <b>1st place</b> in{" "}
-        <b>HackQuest</b>, consistently ranking in the <b>top 5%</b> across
-        competitive platforms.
+        <b>peak rating of 1955</b>.
         <br />
         <br />
         My development experience centers around building high-performance,
@@ -49,7 +23,7 @@ const AboutMeText = () => {
         <b>CODELAB</b> — an AI-powered collaborative code editor — and{" "}
         <b>CODEX NETWORK</b>— a real-time programming community platform —
         demonstrate my expertise in <b>React.js</b>, <b>Node.js</b>,{" "}
-        <b>WebSockets</b>, and <b>OpenAI API integration</b>.
+        <b>WebSockets</b>, and <b>API integration</b>.
         <br />
         <br />I also have deep hands-on experience with <b>Java</b> and{" "}
         <b>C++</b> for algorithmic problem-solving, and I regularly apply these
@@ -63,12 +37,37 @@ const AboutMeText = () => {
         <b>Redux Toolkit</b>, and mentoring peers. In my spare time, I express
         creativity through <b>poetry</b> and <b>technical blogging</b>, sharing
         insights with the broader dev community.
+      </p> */}
+
+      <p className="text-justify">
+        I’m <b>Ashish Soni</b>, a passionate <b>Full Stack Web Developer</b>{" "}
+        with a strong grasp of <b>Data Structures & Algorithms</b> and the{" "}
+        <b>MERN Stack</b>. I’ve solved over <b>1300+ DSA problems</b>, hold a{" "}
+        <b>LeetCode Knight Badge</b>, and reached a <b>peak rating of 1955</b>.
+        <br />
+        <br />
+        My development experience centers around building high-performance,
+        scalable applications using the <b>MERN stack</b>. Projects like{" "}
+        <b>CODELAB</b> — an AI-powered collaborative code editor — and{" "}
+        <b>CODEX NETWORK</b>— a real-time programming community platform —
+        demonstrate my expertise in <b>React.js</b>, <b>Node.js</b>,{" "}
+        <b>WebSockets</b>, and <b>API integration</b>.
+        <br />
+        <br />I also bring strong proficiency in <b>Java</b> and <b>C++</b> for
+        problem-solving, and have led <b>DSA workshops</b> as a{" "}
+        <b>Competitive Programming Lead</b> at{" "}
+        <b>Google Developers Group GLBITM</b>.
+        <br />
+        <br />
+        Outside of coding, I enjoy crafting <b>UI/UX designs</b>, exploring{" "}
+        <b>Redux Toolkit</b>, writing <b>technical blogs</b>, and sharing ideas
+        through <b>poetry</b>.
       </p>
 
       <div className="flex gap-4 mt-5">
         <LinkPreview
           // url="https://www.linkedin.com/in/ashishxsoni"
-          imageSrc="/images/Linkedin-2.png"
+          imageSrc="/images/Linkedin.png"
           isStatic={true}
           height={325}
           width={300}
